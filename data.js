41e68fc56d5142930381adfe7531140441d35dbd
Irem Karakaya <ik2403@LAB-511B-08.local>
2017-03-07 12:00:24 -0500

    Added new data file.

---
+++ data.js
@@ -0,0 +1,20 @@
+/**
+ * @author Irem Karakaya
+ */
+
+
+var chart1Data = [
+[ ' Afghanistan ' , 237069, 'label1'] ,
+ [ ' Albania ' , 104, 'label2'] ,
+ [ ' Algeria ' , 94182,'label3'] ,
+ [ ' Angola ' , 15555,'label4'] ,
+ [ ' Anguilla ' , 1, 'label5'] ,
+ [ ' Antigua and Barbuda ' , 15, 'label6'] ,
+ [ ' Argentina ' , 3207, 'label7'] ,
+ [ ' Armenia ' , 3319, 'label8'] ,
+ [ ' Aruba ' , 2, 'label9'] ,
+ [ ' Australia ' , 36917, 'label10'] 
+ ];
+
+ 
+var chart2Data = [['FY  2010', 4444], ['FY  2011', 3933], ['FY  2012', 10146], ['FY  2013', 20805], ['FY  2014', 51705], ['FY  2015', 28387], ['FY  2016', 46893]];
